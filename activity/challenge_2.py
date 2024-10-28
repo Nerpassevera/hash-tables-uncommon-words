9db9d50eb17496bf25c152f38c1444c4ae40a65d
Tatiana Trofimova <nerpassevera@gmail.com>
2024-10-28 11:52:28 -0700

    Solved

--- activity/challenge_2.py
+++ activity/challenge_2.py
@@ -1,2 +1,16 @@
-def uncommon_from_sentences(sentences):
-    pass
+def count_dict_generator(array:list, table: dict):
+    for item in array:
+        table[item] = table.get(item, 0) + 1
+
+def uncommon_from_sentences(sentences:list):
+
+    result = []
+    word_table = {}
+    for sentence in sentences:
+        count_dict_generator(set(sentence.split()), word_table)
+
+    for word, count in word_table.items():
+        if count == 1:
+            result.append(word)
+
+    return result
--- activity/original.py
+++ activity/original.py
@@ -1,2 +1,18 @@
+def count_dict_generator(array:list, table: dict):
+    for item in array:
+        table[item] = table.get(item, 0) + 1
+
 def uncommon_from_sentences(s1, s2):
-    pass
+    l1 = s1.split(" ")
+    l2 = s2.split(" ")
+    result = []
+
+    word_table = {}
+    count_dict_generator(l1, word_table)
+    count_dict_generator(l2, word_table)
+
+    for word, count in word_table.items():
+        if count == 1:
+            result.append(word)
+
+    return result
